457939663e02e92f1472d6773300b589f5103a58
Mircea <mihail.mircea12@gmail.com>
2026-05-12 17:54:56 +0200

    introduce separate task for blinking led

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,19 @@ use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
-    info!("Launching info");
+// Declare async tasks
+#[embassy_executor::task]
+async fn blink_gyrb(
+    mut green: Output<'static>,
+    mut yellow: Output<'static>,
+    mut red: Output<'static>,
+    mut blue: Output<'static>,
+) {
 
     let mut counter = 0;
-    let mut green = Output::new(p.PD12, Level::Low, Speed::Medium);
-    let mut yellow = Output::new(p.PD13, Level::Low, Speed::Medium);
-    let mut red = Output::new(p.PD14, Level::Low, Speed::Medium);
-    let mut blue = Output::new(p.PD15, Level::Low, Speed::Medium);
-
     let wait_time = 100;
+
+    info!("Launching blink loop");
     loop {
         counter = (counter + 1) % (1 << 4);
         info!("c: {}", counter);
@@ -28,6 +29,25 @@ async fn main(_spawner: Spawner) {
         yellow.set_level(((counter >> 2) & 1 == 1).into());
         green.set_level(((counter >> 3) & 1 == 1).into());
 
+        // Timekeeping is globally available, no need to mess with hardware timers.
         Timer::after_millis(wait_time).await;
+    }
+}
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+
+    let green = Output::new(p.PD12, Level::Low, Speed::Medium);
+    let yellow = Output::new(p.PD13, Level::Low, Speed::Medium);
+    let red = Output::new(p.PD14, Level::Low, Speed::Medium);
+    let blue = Output::new(p.PD15, Level::Low, Speed::Medium);
+
+    Spawner::spawn(&_spawner, blink_gyrb(green, yellow, red, blue)).unwrap();
+
+    info!("Launching main loop");
+    loop {
+        info!("hello from main");
+        Timer::after_millis(500).await;
     }
 }
